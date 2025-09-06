1eb916a0ccde7d3ceb67930c2f17faa83df95a17
AdolfoGayoso <a.gayoso02@ufromail.cl>
2025-09-05 23:24:45 -0400

    Commit inicial

---
+++ src/main/java/com/mucho_peso/espectro/EspectroApplication.java
@@ -0,0 +1,13 @@
+package com.mucho_peso.espectro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspectroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspectroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mucho_peso/espectro/EspectroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mucho_peso.espectro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspectroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
